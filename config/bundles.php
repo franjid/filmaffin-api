7daa93df6f2271be8c34908779c943a7bcb3e583
Fran Iruela <franjid@gmail.com>
2020-04-07 14:31:20 +0200

    Removed not needed dependency

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
