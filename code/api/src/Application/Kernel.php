01526ed73656a4cc7ce1abc5f0a4acc1bf6ed4b1
Fran Iruela <franjid@gmail.com>
2020-06-02 16:08:53 +0200

    sync-recipes

--- code/api/src/Application/Kernel.php
+++ code/api/src/Application/Kernel.php
@@ -8,8 +8,6 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
-use function dirname;
-use const PHP_VERSION_ID;
 
 class Kernel extends BaseKernel
 {
@@ -35,7 +33,7 @@ class Kernel extends BaseKernel
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', PHP_VERSION_ID < 70400 || $this->debug);
+        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir() . '/config';
 
