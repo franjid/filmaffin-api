eeb5137a6902b94d01576d931ee66fe38102a4fe
Fran Iruela <franjid@gmail.com>
2023-02-08 22:53:53 +0100

    Add new endpoint for ads config

---
+++ code/api/src/Application/Controller/AdsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Application\Controller;
+
+use Nelmio\ApiDocBundle\Annotation\Operation;
+use Swagger\Annotations as SWG;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AdsController extends AbstractController
+{
+    /**
+     * @Operation(
+     *     tags={"Ads"},
+     *     summary="Get ads config",
+     *     @SWG\Response(
+     *         response="200",
+     *         description=""
+     *     ),
+     * )
+     *
+     * @return JsonResponse
+     */
+    public function configAction(): JsonResponse
+    {
+        return new JsonResponse([
+            "enabled" => true,
+            "num_views_before_show_add" => 10,
+        ],
+            JsonResponse::HTTP_OK
+        );
+    }
+}
